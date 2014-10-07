1281cdd49e654ed72a7ee62eae1819ac6d730736
linyows <linyows@gmail.com>
2014-10-07 18:50:00 +0900

    add comment count

--- _jedie/_site/javascripts/random_background_image.js
+++
@@ -1,9 +0,0 @@
-window.onload = function(){
-  var bg = [
-    "/images/gopher_ramen.png",
-    "/images/gopher_niwaka.png"
-  ];
-  var r = Math.floor(Math.random() * bg.length);
-  var el = document.getElementById("random");
-  el.style.backgroundImage = "url(" + bg[r] + ")";
-};
